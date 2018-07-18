c7fbc6b6b3890381f03d667df1bf8e78f677e6ac
Charles Bergeron <chuck.bergeron@gmail.com>
2018-07-18 10:46:48 -0700

    * add api file remove back to the local 'up' script

--- up.sh
+++ up.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 mkdir -p /docker/etc/nginx/conf.d
+rm -f /docker/var/ipfs/data/api
 cp nginx.conf.d/dev-proxy.conf /docker/etc/nginx/conf.d/proxy.conf
 cd docker
 docker-compose up --build
